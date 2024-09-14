f5d12787d826d6839e32f7c7ee4a064ca42dc39a
AlexPower2 <apowell5395@qccomets.com>
2024-09-14 15:03:44 -0400

    Time Complexity

---
+++ Python/O^/Activity 2.py
@@ -0,0 +1,9 @@
+def Alexcode(n):
+    iteration=0
+    for i in range(n):
+        iteration +=1
+    print("number entered = ",n)
+    print("number of iteration = ",iteration)
+Alexcode(10)
+Alexcode(20)
+
---
+++ Python/O^/Activity 3.py
@@ -0,0 +1,9 @@
+def Morsecode(n):
+    iteration=0
+    for i in range(n):
+        for j in range(n):
+            iteration+=1
+    print("number entered =",n)
+    print("number of iteration =",iteration)
+Morsecode(5)
+Morsecode(20)
