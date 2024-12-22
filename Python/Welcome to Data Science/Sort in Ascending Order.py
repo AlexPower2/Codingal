01a48b373a147d5383e4b7182ddcd0593b36e038
AlexPower2 <apowell5395@qccomets.com>
2024-12-22 13:58:42 -0500

    Code
    Co-authored-by: NikPRcodes <NikPRcodes@users.noreply.github.com>

--- Python/Welcome to Data Science/Sort in Ascending Order.py
+++ Python/Welcome to Data Science/Sort in Ascending Order.py
@@ -1,8 +1,8 @@
 import numpy as np
 data_type=[('name', 'S15'), ('class', int), ('height', float)]
-students_details=[{'James', S, 48.5}, ('Nail', 6, 52.5),('Paul', S, 42.10), ('Pit', S, 40.11)]
-students=np.array(students_details, data_type)
+students_details=[('James', 5, 48.5), ('Nail', 6, 52.5),('Paul', 5, 42.10), ('Pit', 5, 40.11)]
+students=np.array(students_details, dtype=data_type)
 print("Original array: ")
 print(students)
 print("Sort by height")
-print(np.sort(students, 'height'))
+print(np.sort(students, order='height'))
